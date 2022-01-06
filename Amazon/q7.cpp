f1152c3359ffe853a98996d9f538322f69bd9a30
Amartya Choudhary <54511196+AC-BOSS@users.noreply.github.com>
2022-01-06 23:52:15 +0530

    Create q7.cpp

---
+++ Amazon/q7.cpp
@@ -0,0 +1,50 @@
+// Question: First non-repeating character in a stream
+//Question link: https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1#
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+	public:
+		string FirstNonRepeating(string s){
+		    // Code here
+		    int m[26]={0};
+		    queue<char>q;
+		    for(int i=0;i<s.length();i++)
+		    {
+		        m[s[i]-'a']++;
+		        if(m[s[i]-'a']==1)
+		        {
+		            q.push(s[i]);
+		        }
+		        while(!q.empty() && m[q.front()-'a']!=1)
+		        {
+		            q.pop();
+		        }
+		        if(q.empty())
+		        {
+		            s[i]='#';
+		        }
+		        else
+		        {
+		            s[i]=q.front();
+		        }
+		    }
+		    return s;
+		}
+
+};
+
+// { Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		string A;
+		cin >> A;
+		Solution obj;
+		string ans = obj.FirstNonRepeating(A);
+		cout << ans << "\n";
+	}
+	return 0;
+}  // } Driver Code Ends
